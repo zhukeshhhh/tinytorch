41c575d8e6fe75a74e0964051e9dee75017a5ad2
Nurzhan Zhukesh <nurzhan.zhukesh@gmail.com>
2026-06-12 08:06:25 +0200

    some changes in arch

--- core/matrix.cpp
+++ core/matrix.cpp
@@ -2,29 +2,60 @@
 #include <iostream>
 #include <stdexcept>
 #include <cmath>
+#include <vector>
 
-struct matrix
-{
+class Matrix {
+public:
     float* _values;
     std::size_t _rows;
     std::size_t _cols;
+    std::vector<std::size_t> _mshape;
 
-    matrix(
+    Matrix(
         std::size_t rows,
         std::size_t cols
     ) : _rows{rows},
         _cols{cols}
     {
-        _values = (float*)malloc(sizeof(float) * rows * cols);
+        _mshape = {rows, cols};
+        _values = new float[rows * cols]{};
+    }
+
+    Matrix(std::size_t size)
+        : _rows{1}, _cols{size}
+    {
+        _values = new float[size]; // no values
+    }
+
+    Matrix(const Matrix& other)
+        : _rows{other._rows}, _cols{other._cols}
+    {
+        _values = new float[_rows * _cols]; // no values
+        std::copy(other._values, other._values + (_cols * _rows), _values);
+    }
+
+    std::size_t getSize() {
+        return _rows * _cols;
+    }
+
+    std::vector<std::size_t> getShape() {
+        return _mshape;
     }
 };
 
+
 int main() {
 
-    matrix m = matrix(3, 4);
-    
-    std::cout << m._rows;
+    Matrix m(3, 5);
+
+    for (int i = 0; i < m._rows; i++) {
+        for (int j = 0; j < m._cols; j++) {
+            std::cout << m._values[i * m._cols + j];
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << m._mshape[0] << " " << m._mshape[1] << std::endl;
 
     return 0;
 }
-
--- core/tensor.cpp
+++ core/tensor.cpp
@@ -4,16 +4,16 @@
 #include <memory>
 #include <stdexcept>
 #include <cmath>
+#include <random>
 #include "matrix.cpp"
 
-class Tensor {
+class Tensor : public std::enable_shared_from_this<Tensor> {
 
 private:
-    float* _data;
-    std::size_t _rows;
-    std::size_t _cols;
+    Matrix* _data;
+    std::vector<std::size_t> _shape;
     float* _grad;
-    std::function<void(const float*)> _gradfn;
+    std::function<void(const Matrix&)> _gradfn;
     std::vector<std::shared_ptr<Tensor>> _parents;
     bool _requires_grad;
 
@@ -22,58 +22,80 @@ public:
     Tensor(
         float data,
         bool requires_grad = false,
-        std::function<void(const float*)> gradfn = nullptr,
+        std::function<void(const Matrix&)> gradfn = nullptr,
         std::vector<std::shared_ptr<Tensor>> parents = {}
-    ) : _data{new float{data}},
-        _rows{1},
-        _cols{1},
+    ) : _data{new Matrix(1, 1)},
+        _shape{1, 1},
         _grad{nullptr},
         _gradfn{gradfn},
         _parents{parents},
         _requires_grad{requires_grad}
     {
-        // implement constructor for single-value Tensor
+        _data->_values[0] = data;
     }
 
-    // vector Tensor : row vector by default
+    // 1D Tensor
+
     Tensor(
-        //float* data,
-        std::size_t size,
+        Matrix& data,
         bool requires_grad = false,
-        std::function<void(const float*)> gradfn = nullptr,
+        std::function<void(const Matrix&)> gradfn = nullptr,
         std::vector<std::shared_ptr<Tensor>> parents = {}
-    ) : //_data{new float[size]},
-        _rows{1},
-        _cols{size},
+    ) : _data{new Matrix(data)},
+        _shape{data.getShape()},
         _grad{nullptr},
         _gradfn{gradfn},
         _parents{parents},
         _requires_grad{requires_grad}
     {
-        _data = (float*)malloc(sizeof(float) * size);
+        
     }
 
     Tensor(
-        float* data,
-        std::size_t rows,
-        std::size_t cols,
+        Matrix& data,
         bool requires_grad = false,
-        std::function<void(const float*)> gradfn = nullptr,
+        std::function<void(const Matrix&)> gradfn = nullptr,
         std::vector<std::shared_ptr<Tensor>> parents = {}
-    ) : _data{new float[rows * cols]},
-        _rows{rows},
-        _cols{cols},
+    ) : _data{new Matrix(data)},
+        _shape{data.getShape()},
         _grad{nullptr},
         _gradfn{gradfn},
         _parents{parents},
         _requires_grad{requires_grad}
     {
-        for (std::size_t i = 0; i < rows * cols; i++) {
-            _data[i] = data[i];
+
+    }
+
+    float& item() {
+        if (_data->getSize() != 1) {
+            throw std::runtime_error("item() only works for single-value Tensors\n");
         }
+        return _data->_values[0];
+    }
+
+    float& operator()(std::size_t i) {
+        if (_shape[0] != 1) {
+            throw std::runtime_error("Single indexing only works for 1D Tensors\n");
+        }
+        if (i >= _shape[0]) {
+            throw std::runtime_error("Index is out of bounds");
+        }
+        return _data->_values[i];
+    }
+
+    float& operator()(std::size_t i, std::size_t j) {
+        if (_shape[0] == 1 || _shape[1] == 1) {
+            throw std::runtime_error("Double indexing only works on 2D Tensors\n");
+        }
+        if (i >= _shape[0] || j >= _shape[1]) {
+            throw std::runtime_error("Index out of bounds\n");
+        }
+        return _data->_values[i + _shape[1] + j];
     }
 
     ~Tensor() {
+        delete[] _data;
+        delete[] _grad;
         std::cout << "Tensor destroyed" << std::endl;
     }
 };
@@ -81,18 +103,8 @@ public:
 
 int main() {
 
-    float* data = (float*)malloc(sizeof(float) * 100);
-    for (std::size_t i = 0; i < sizeof(data); i++) {
-        data[i] = i;
-    }
+    std::shared_ptr<Tensor> t1 = std::make_shared<Tensor>(5.0f, false);
+    std::cout << t1->item() << std::endl;
 
-    std::shared_ptr<Tensor> ptr1 = std::make_shared<Tensor>(5, false);
-
-    {
-        std::shared_ptr<Tensor> ptr2 = ptr1;
-        std::cout << "ptr2 inner scope use count = " << ptr2.use_count() << std::endl;
-        std::cout << "ptr1 inner scope use count = " << ptr1.use_count() << std::endl;    
-    }
-
-    std::cout << "ptr1 global scope use count = " << ptr1.use_count() << std::endl;
+    return 0;
 }
